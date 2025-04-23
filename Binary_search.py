5ef02baaeeee6e193001a39b72a0d1ad60dd2ad4
Rajashree <rajashree3101@gmail.com>
2025-04-23 10:42:10 -0400

    LC 704

---
+++ Binary_search.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            m = (l+r)//2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return -1
