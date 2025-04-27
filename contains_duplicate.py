2f96b3265a10377c835362fc72e43c33b73729d6
Rajashree <rajashree3101@gmail.com>
2025-04-26 21:41:45 -0400

    lc 217

---
+++ contains_duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hs=set()
+
+        for n in nums:
+            if n in hs:
+                return True
+            hs.add(n)
+        return False
