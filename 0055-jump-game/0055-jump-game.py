428d112b8364c3972c3983293c1702b843bacfce
Rajashree <rajashree3101@gmail.com>
2025-08-04 10:37:20 -0400

    Time: 19 ms (67.6%), Space: 18.8 MB (13.43%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n=len(nums)
+        target=n-1
+        for i in range(n-1,-1,-1):
+            max_jump=nums[i]
+            if i+max_jump >=target:
+                target=i
+        return target==0
+        
