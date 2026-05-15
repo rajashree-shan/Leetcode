340a430ba07c1fa54e46add1f486f99a369af634
Rajashree <rajashree3101@gmail.com>
2026-05-14 23:58:58 -0400

    Time: 58 ms (98.89%), Space: 19.6 MB (50.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left = 0  # Start of the sliding window
-        max_count = 0  # To track the count of the most frequent character in the window
-        char_count = {}  # Dictionary to store the frequency of characters
-        max_length = 0  # To store the maximum length of the substring
+        cnt={}
+        left=0
+        maxi=0
+        ans=0
 
-        for right in range(len(s)):
-            # Update the frequency of the current character
-            char_count[s[right]] = char_count.get(s[right], 0) + 1
+        for right,num in enumerate(s):
+            cnt[num]=cnt.get(num,0)+1
+            maxi=max(maxi,cnt[num])
 
-            # Track the most frequent character count in the current window
-            max_count = max(max_count, char_count[s[right]])
+            if right-left+1 -maxi > k:
+                cnt[s[left]]-=1
+                left+=1
+        ans = max(ans, right - left + 1)
 
-            # If the number of replacements needed exceeds k, shrink the window
-            while (right - left + 1) - max_count > k:
-                char_count[s[left]] -= 1
-                left += 1
-
-            # Update the maximum length of the valid substring
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
+        return ans
