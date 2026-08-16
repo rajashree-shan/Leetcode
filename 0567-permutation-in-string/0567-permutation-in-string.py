37c340699a6218ed6ba5527c16d9229065bcf8b1
Rajashree <rajashree3101@gmail.com>
2026-08-16 14:49:38 -0700

    Time: 11 ms (96.36%), Space: 19.4 MB (53.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,23 @@
-from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_count=Counter(s1)
-        s1_l=len(s1)
-        s2_l=len(s2)
-        if s1_l > s2_l:
+        if len(s1) > len(s2):
             return False
-        window_count = Counter(s2[:s1_l])
-        if window_count == s1_count:
-            return True 
-                # Slide the window across s2
-        for i in range(s1_l, s2_l):
-            # Remove the leftmost character from the window
-            left_char = s2[i - s1_l]
-            window_count[left_char] -= 1
-            if window_count[left_char] == 0:
-                del window_count[left_char]
-            
-            # Add the new character to the window
-            right_char = s2[i]
-            window_count[right_char] += 1
-            
-            # Check if the current window matches s1_count
-            if window_count == s1_count:
+        n1=len(s1)
+        n2=len(s2)
+        count1=[0]*26
+        count2=[0]*26
+        for i in range(len(s1)):
+            count1[ord(s1[i])-ord('a')]+=1
+            count2[ord(s2[i])-ord('a')]+=1
+
+        if count1==count2:
+            return True
+        for i in range(n1,n2):
+            count2[ord(s2[i])-97]+=1
+            count2[ord(s2[i-n1]) - 97]-=1
+            if count1==count2:
                 return True
+
         return False
+        
+
